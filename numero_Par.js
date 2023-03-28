1c7d025171f6bc980bdf3ce418fd8e8fa0f5cbb3
Nero Silva <125216211+nerosilva@users.noreply.github.com>
2023-03-28 16:37:06 -0300

    Add files via upload

---
+++ numero_Par.js
@@ -0,0 +1,14 @@
+//exercicio: verificar si e par ou impar
+
+// 1. pedir ao usuario para digitar um numero
+const numero = parseInt(prompt("digite um numero:"));
+ 
+// 2. verificar se o numero é par ou impar
+ if(numero% 2 ===0) {
+ console.log(" o numero" + numero + " é par.");
+
+ } else{
+
+    console.log("o numero" + numero + "é impar.");
+ }
+
---
+++ o_chamar funçao.js
@@ -0,0 +1,6 @@
+function Somar(num1,num2) {
+   return num1 + num2;
+}
+
+ SOMAR (5,5)
+
